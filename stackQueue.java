8098556cf2fa24f4cf8af7babe84ba401991c352
bossman48 <okuzucu@etu.edu.tr>
2020-02-23 12:25:54 +0300

    Stack and Queue

---
+++ stackQueue.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class stackQueue {
+	public static void main(String[] args) {
+            Stack<Integer> asd=new Stack<>();
+            asd.push(12);
+            asd.push(14);
+            System.out.println(asd.pop());
+    }  
+
+}
